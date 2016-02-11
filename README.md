e31c82986d3a75e66c1661b8ba128d861cf3e510
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2016-02-12 00:17:03 +0300

    Add sdl2 as a dependency

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # old-rusty-platforms
 My attempt to make an emulator
+
+## Dependencies
+### Ubuntu:
+- `libsdl2-2.0-0`
+
+## Development dependencies
+### Ubuntu:
+- `libsdl2-dev`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod binary_parser;
 mod chip8;
 extern crate rand;
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 fn main() {
     let mut chip: chip8::CHIP8 = chip8::CHIP8::new();
@@ -8,4 +13,33 @@ fn main() {
     chip.print_first_16_bytes_of_ram();
     let x: u8 = rand::random();
     println!("{}", x);
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+
+    renderer.set_draw_color(Color::RGB(0, 0, 255));
+    renderer.clear();
+    renderer.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+    }
 }
